2226bd5161a8c8016396c55b2af446076db69ab0
0736.kim <18042542@qq.com>
2019-03-08 12:58:56 +0800

    modified:   README.md 	modified:   bin/config.toml 	modified:   src/control/admin/config.go

--- src/control/admin/config.go
+++ src/control/admin/config.go
@@ -2,7 +2,6 @@ package admin
 
 import (
 	"github.com/gin-gonic/gin"
-	"logic"
 	"strconv"
 	"strings"
 )
@@ -11,21 +10,19 @@ import (
 func (this *AdminControl) ConfigInfo (c *gin.Context) {
 	if strings.ToUpper(c.Request.Method) == "POST" {
 		_type, _ := strconv.Atoi(c.DefaultPostForm("type", "0"))
+		iRelust := 1003
 		if _type == 1 {
 
 		} else if _type == 2 {
-
+			content := make(map[string]interface{})
+			content["platform"] = c.DefaultPostForm("platform", "")
+			content["record_no"] = c.DefaultPostForm("record_no", "")
+			content["wx"] = c.DefaultPostForm("wx", "")
+			content["tel"] = c.DefaultPostForm("tel", "")
+			content["address"] = c.DefaultPostForm("address", "")
 		} else {
 			returnJson(c,1001,nil)
 		}
-		platform := c.DefaultPostForm("platform", "")
-		record_no := c.DefaultPostForm("record_no", "")
-		wx := c.DefaultPostForm("wx", "")
-		tel := c.DefaultPostForm("tel", "")
-		address := c.DefaultPostForm("address", "")
-		
-
-		iRelust := logic.GetSysMenu().GetMenuListByPage(int32(page), keyword)
 		returnJson(c, 0, iRelust)
 		return
 	}
